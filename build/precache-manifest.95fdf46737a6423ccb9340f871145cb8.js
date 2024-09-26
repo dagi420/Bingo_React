afadba007f8b9006f19b14c57010e7d52d7789cd
dagi420 <dagiovoxo@gmail.com>
2024-09-26 04:04:29 +0300

    reinstalled after removing node module and configured package.json to run with ssl

---
+++ build/precache-manifest.95fdf46737a6423ccb9340f871145cb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200f757ad560c8214b8873c706cab0dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83dd0c80489e911f8a99",
+    "url": "/static/css/main.44bdc4b7.chunk.css"
+  },
+  {
+    "revision": "f83fc4120e33371711c3",
+    "url": "/static/js/2.9ca80a9f.chunk.js"
+  },
+  {
+    "revision": "b7a72720654882edb7697daeb9cf8e86",
+    "url": "/static/js/2.9ca80a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f7f0a2ff2f5b561d21",
+    "url": "/static/js/3.47f7437e.chunk.js"
+  },
+  {
+    "revision": "83dd0c80489e911f8a99",
+    "url": "/static/js/main.a1b90e13.chunk.js"
+  },
+  {
+    "revision": "6b6d5373ccbebd0b3dfe",
+    "url": "/static/js/runtime-main.1719518a.js"
+  }
+]);
