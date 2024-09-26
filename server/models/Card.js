8dc0eed3c3ca765f2c3301621c465b2134b2ae8a
dagi420 <dagiovoxo@gmail.com>
2024-09-26 03:18:49 +0300

    delete server

--- server/models/Card.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const CardSchema = new mongoose.Schema({
-  numbers: [[Number]],
-  isChosen: { type: Boolean, default: false }
-});
-
-module.exports = mongoose.model('Card', CardSchema);
